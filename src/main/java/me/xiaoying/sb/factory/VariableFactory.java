b3e1b569a6ea36b7ffc1a3ef9ed5f9c1d74df3c5
Hanhan <764932129@qq.com>
2023-06-24 15:33:58 +0800

    feat: use VariableFactory to set content

---
+++ src/main/java/me/xiaoying/sb/factory/VariableFactory.java
@@ -0,0 +1,47 @@
+package me.xiaoying.sb.factory;
+
+
+import me.clip.placeholderapi.PlaceholderAPI;
+import me.xiaoying.sb.utils.ColorUtil;
+import me.xiaoying.sb.utils.DateUtil;
+import org.bukkit.entity.Player;
+
+/**
+ * 变量工厂
+ */
+public class VariableFactory {
+    String string;
+
+    public VariableFactory(String string) {
+        this.string = string;
+    }
+
+    public VariableFactory player(Player player) {
+        this.string = this.string.replace("%player%", player.getName());
+        return this;
+    }
+
+    public VariableFactory date(String dateFormat) {
+        this.string = this.string.replace("%date%", DateUtil.getDate(dateFormat));
+        return this;
+    }
+
+    public VariableFactory prefix(String prefix) {
+        this.string = this.string.replace("%prefix%", prefix);
+        return this;
+    }
+
+    public VariableFactory placeholder(Player player) {
+        this.string = PlaceholderAPI.setPlaceholders(player, this.string);
+        return this;
+    }
+
+    public VariableFactory color() {
+        this.string = ColorUtil.translate(this.string);
+        return this;
+    }
+
+    public String getString() {
+        return this.string;
+    }
+}
