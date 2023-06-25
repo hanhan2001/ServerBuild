f131e3b2abcce02f687316ab3775f19b9379d525
LittleK <42397439+TomGiHub@users.noreply.github.com>
2023-06-25 08:30:27 +0800

    Add files via upload

---
+++ src/main/java/me/xiaoying/sb/constant/NotBuildConstant.java
@@ -0,0 +1,23 @@
+package me.xiaoying.sb.constant;
+
+import java.util.List;
+
+public class NotBuildConstant {
+    public static boolean
+            SET_ENABLE,
+            SET_BUILD_ENABLE,
+            SET_DESTRUCTION_ENABLE;
+    public static String SET_DATEFORMAT;
+    public static List<String>
+            MESSAGE_BUILD,
+            MESSAGE_DESTRUCTION,
+            MESSAGE_HELP;
+    public static List<String>
+            BUILD_WORLDS,
+            DESTRUCTION_WORLDS;
+
+    public static String
+            MESSAGE_PREFIX,
+            MESSAGE_RELOAD,
+            MESSAGE_NOPERMISSION;
+}
