079b5083e9b4df1d60d52bd3b8b9c67e6bd91ae4
LittleK <42397439+TomGiHub@users.noreply.github.com>
2023-07-11 08:59:36 +0800

    Add files via upload

---
+++ src/main/java/me/xiaoying/sb/constant/HomeConstant.java
@@ -0,0 +1,25 @@
+package me.xiaoying.sb.constant;
+
+import java.util.List;
+
+public class HomeConstant {
+    public static boolean
+            SET_ENABLE,
+            SET_WORLDLIMITS_ENABLE;
+
+    public static String SET_DATEFORMAT;
+
+    public static String HOMENAMEREGEX;
+
+    public static List<String>
+            WORLDLIMITS_WORLD,
+            HOME_GROUPS;
+
+    public static List<String>
+            MESSAGE_HELP;
+
+    public static String
+            MESSAGE_PREFIX,
+            MESSAGE_RELOAD,
+            MESSAGE_NOPERMISSION;
+}
