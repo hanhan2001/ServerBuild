b351f3bbe6425c30b064faac3dff6b360bf43763
Hanhan <764932129@qq.com>
2023-12-01 22:10:10 +0800

    feat: ScriptManager make done

--- src/main/java/me/xiaoying/serverbuild/script/ScriptManager.java
+++ src/main/java/me/xiaoying/serverbuild/script/ScriptManager.java
@@ -1,15 +1,21 @@
 package me.xiaoying.serverbuild.script;
 
+import me.xiaoying.serverbuild.ServerBuild;
+import me.xiaoying.serverbuild.script.scripts.TitleScript;
+import me.xiaoying.serverbuild.utils.ServerUtil;
 import org.bukkit.plugin.Plugin;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * ScriptManager
  */
 public class ScriptManager {
     Map<String, Script> knownScript = new HashMap<>();
+
+    public ScriptManager() {
+        this.registerScript(new TitleScript(), ServerBuild.getInstance());
+    }
 
     /**
      * 注册 Script
@@ -18,16 +24,75 @@ public class ScriptManager {
      * @param plugin 来源插件
      */
     public void registerScript(Script script, Plugin plugin) {
+        String command = plugin.getName().toLowerCase() + ":" + script.command();
 
+        if (this.knownScript.containsKey(command))
+            return;
+
+        this.knownScript.put(command, script);
+
+        // 别名
+        if (script.alias() == null)
+            return;
+
+        for (String alias : script.alias()) {
+            if (this.knownScript.containsKey(plugin.getName().toLowerCase() + ":" + alias))
+                continue;
+
+            this.knownScript.put(plugin.getName().toLowerCase() + ":" + alias, script);
+        }
     }
 
-    public void unregister(Script script) {
-
+    /**
+     * 取消注册 Script
+     *
+     * @param script Script
+     */
+    public void unregisterScript(Script script) {
+        this.knownScript.values().removeIf(script1 -> script1 == script);
     }
 
-    public void unregister(String command) {
-
+    /**
+     * 取消注册 Script
+     *
+     * @param script Script 命令名称
+     * @param plugin Plugin
+     */
+    public void unregisterScript(String script, Plugin plugin) {
+        this.knownScript.remove(plugin.getName().toLowerCase() + ":" + script);
     }
 
+    /**
+     * 执行 Script
+     * 使用线程池
+     *
+     * @param command String
+     */
+    public void callScript(String command) {
+        ServerBuild.getTaskService().getScheduledExecutor().execute(() -> onCommand(command));
+    }
 
+    /**
+     * 执行 Script
+     *
+     * @param command String
+     */
+    public void onCommand(String command) {
+        command = command.toLowerCase();
+        String[] split = command.split(" ");
+
+        // 命令头处理
+        String head = split[0];
+        if (!head.contains(":"))
+            head = "serverbuild:" + head;
+
+        // 判断是否存在 Script 命令
+        if (!this.knownScript.containsKey(head)) {
+            ServerUtil.sendMessage("&c未知命令 &e" + head.split(":")[head.split(":").length - 1]+ " &c请检查配置文件", true);
+            return;
+        }
+
+        String[] function = new ArrayList<>(Arrays.asList(split).subList(1, split.length)).toArray(new String[0]);
+        this.knownScript.get(head).performCommand(function);
+    }
 }
