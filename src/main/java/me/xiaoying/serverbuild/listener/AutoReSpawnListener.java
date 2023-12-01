28a37e40baedabdb2f01cffbbdf8b20fb1eae286
Hanhan <764932129@qq.com>
2023-12-01 22:11:49 +0800

    feat: set AutoReSpawn support ServerBuild Script

--- src/main/java/me/xiaoying/serverbuild/listener/AutoReSpawnListener.java
+++ src/main/java/me/xiaoying/serverbuild/listener/AutoReSpawnListener.java
@@ -2,6 +2,7 @@ package me.xiaoying.serverbuild.listener;
 
 import me.xiaoying.serverbuild.ServerBuild;
 import me.xiaoying.serverbuild.constant.ConstantAutoReSpawn;
+import me.xiaoying.serverbuild.factory.VariableFactory;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
@@ -14,6 +15,17 @@ import java.util.concurrent.TimeUnit;
 public class AutoReSpawnListener implements Listener {
     @EventHandler
     public void onPlayerDeath(PlayerDeathEvent event) {
-        ServerBuild.getTaskService().getScheduledExecutor().schedule(() -> event.getEntity().spigot().respawn(), ((ConstantAutoReSpawn) ServerBuild.getFunctionService().getFunction("AutoReSpawn").getFiles().get(0).getConstant()).AUTORESPAWN_TICK, TimeUnit.MILLISECONDS);
+        ServerBuild.getTaskService().getScheduledExecutor().schedule(() -> {
+            event.getEntity().spigot().respawn();
+
+            ConstantAutoReSpawn constantAutoReSpawn = (ConstantAutoReSpawn) ServerBuild.getFunctionService().getFunction("AutoReSpawn").getFiles().get(0).getConstant();
+            for (String s : constantAutoReSpawn.AUTORESPAWN_EVENT) {
+                s = new VariableFactory(s)
+                        .player(event.getEntity().getName())
+                        .color()
+                        .toString();
+                ServerBuild.getScriptManager().onCommand(s);
+            }
+        }, ((ConstantAutoReSpawn) ServerBuild.getFunctionService().getFunction("AutoReSpawn").getFiles().get(0).getConstant()).AUTORESPAWN_TICK, TimeUnit.MILLISECONDS);
     }
 }
--- src/main/java/me/xiaoying/serverbuild/task/tasks/AutoReSpawnTask.java
+++ src/main/java/me/xiaoying/serverbuild/task/tasks/AutoReSpawnTask.java
@@ -2,6 +2,7 @@ package me.xiaoying.serverbuild.task.tasks;
 
 import me.xiaoying.serverbuild.ServerBuild;
 import me.xiaoying.serverbuild.constant.ConstantAutoReSpawn;
+import me.xiaoying.serverbuild.factory.VariableFactory;
 import me.xiaoying.serverbuild.task.SubTask;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
@@ -30,6 +31,15 @@ public class AutoReSpawnTask implements SubTask {
                     continue;
 
                 onlinePlayer.spigot().respawn();
+
+                ConstantAutoReSpawn constantAutoReSpawn = (ConstantAutoReSpawn) ServerBuild.getFunctionService().getFunction("AutoReSpawn").getFiles().get(0).getConstant();
+                for (String s : constantAutoReSpawn.AUTORESPAWN_EVENT) {
+                    s = new VariableFactory(s)
+                            .player(onlinePlayer.getName())
+                            .color()
+                            .toString();
+                    ServerBuild.getScriptManager().onCommand(s);
+                }
             }
         }, 0L, ((ConstantAutoReSpawn) ServerBuild.getFunctionService().getFunction("AutoReSpawn").getFiles().get(0).getConstant()).AUTORESPAWN_TICK);
     }
