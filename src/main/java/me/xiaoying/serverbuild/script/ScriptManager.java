0e2aa8098bde49e63cfce0aea262340cab10844e
Hanhan <764932129@qq.com>
2023-12-02 14:59:10 +0800

    fix: ActionbarScript useless

--- src/main/java/me/xiaoying/serverbuild/script/ScriptManager.java
+++ src/main/java/me/xiaoying/serverbuild/script/ScriptManager.java
@@ -1,6 +1,7 @@
 package me.xiaoying.serverbuild.script;
 
 import me.xiaoying.serverbuild.ServerBuild;
+import me.xiaoying.serverbuild.script.scripts.ActionbarScript;
 import me.xiaoying.serverbuild.script.scripts.SendScript;
 import me.xiaoying.serverbuild.script.scripts.TitleScript;
 import me.xiaoying.serverbuild.utils.ServerUtil;
@@ -17,6 +18,7 @@ public class ScriptManager {
     public ScriptManager() {
         this.registerScript(new SendScript(), ServerBuild.getInstance());
         this.registerScript(new TitleScript(), ServerBuild.getInstance());
+        this.registerScript(new ActionbarScript(), ServerBuild.getInstance());
     }
 
     /**
