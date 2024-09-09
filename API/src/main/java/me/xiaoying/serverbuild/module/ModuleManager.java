0fab842e3feca4b7278a56088f3b55507e98c67d
Hanhan <764932129@qq.com>
2024-09-09 21:49:55 +0800

    feat: make notes for ModuleManager

--- API/src/main/java/me/xiaoying/serverbuild/module/ModuleManager.java
+++ API/src/main/java/me/xiaoying/serverbuild/module/ModuleManager.java
@@ -3,17 +3,47 @@ package me.xiaoying.serverbuild.module;
 import java.util.List;
 
 public interface ModuleManager {
+    /**
+     * Register module
+     *
+     * @param module Module
+     */
     void registerModule(Module module);
 
+    /**
+     * Unregister module
+     *
+     * @param module Module
+     */
     void unregisterModule(Module module);
 
+    /**
+     * Unregister all module
+     */
     void unregisterModules();
 
+    /**
+     * Get module by module's alias name
+     *
+     * @param name Module's alias name
+     * @return Module
+     */
     Module getModule(String name);
 
+    /**
+     * Get all registered module
+     *
+     * @return Module list
+     */
     List<Module> getModules();
 
+    /**
+     * Enable all module
+     */
     void enableModules();
 
+    /**
+     * Disable all module
+     */
     void disableModules();
 }
