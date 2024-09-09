2d4db4b615dadbe88fd3b8404eee0f1eaa0c5ccd
Hanhan <764932129@qq.com>
2024-09-09 21:46:40 +0800

    feat: make notes for ScriptManager

--- API/src/main/java/me/xiaoying/serverbuild/script/ScriptManager.java
+++ API/src/main/java/me/xiaoying/serverbuild/script/ScriptManager.java
@@ -2,7 +2,6 @@ package me.xiaoying.serverbuild.script;
 
 import me.xiaoying.serverbuild.script.interpreter.InterpreterManager;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
 import java.util.List;
 
@@ -10,17 +9,51 @@ import java.util.List;
  * Script manager
  */
 public interface ScriptManager {
+    /**
+     * Register script
+     *
+     * @param script Script
+     */
     void registerScript(Script script);
 
+    /**
+     * Unregister script by script's name
+     *
+     * @param script Script's name
+     */
     void unregisterScript(String script);
 
+    /**
+     * Unregister script
+     *
+     * @param script Script
+     */
     void unregisterScript(Script script);
 
+    /**
+     * Unregister all script
+     */
     void unregisterScripts();
 
+    /**
+     * Perform script
+     *
+     * @param command full script command
+     * @param sender Who sent this script command
+     */
     void performScript(String command, CommandSender sender);
 
+    /**
+     * Get all registered scripts' name
+     *
+     * @return
+     */
     List<String> getScripts();
 
+    /**
+     * Get script manager's interpreter manager
+     *
+     * @return InterpreterManager
+     */
     InterpreterManager getInterpreterManager();
 }
