88eb559666bdd62d5fd666048570209932baad1d
Hanhan <764932129@qq.com>
2024-09-10 14:41:18 +0800

    feat: make module load

--- API/src/main/java/me/xiaoying/serverbuild/module/ModuleManager.java
+++ API/src/main/java/me/xiaoying/serverbuild/module/ModuleManager.java
@@ -1,5 +1,6 @@
 package me.xiaoying.serverbuild.module;
 
+import java.io.File;
 import java.util.List;
 
 public interface ModuleManager {
@@ -21,6 +22,20 @@ public interface ModuleManager {
      * Unregister all module
      */
     void unregisterModules();
+
+    /**
+     * Load module from file
+     *
+     * @param file file
+     */
+    void loadModule(File file);
+
+    /**
+     * Load modules from folder
+     *
+     * @param folder Folder file
+     */
+    void loadModules(File folder);
 
     /**
      * Get module by module's alias name
--- API/src/main/java/me/xiaoying/serverbuild/module/SimpleModuleManager.java
+++ API/src/main/java/me/xiaoying/serverbuild/module/SimpleModuleManager.java
@@ -1,8 +1,10 @@
 package me.xiaoying.serverbuild.module;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import me.xiaoying.serverbuild.utils.YamlUtil;
+import me.xiaoying.serverbuild.utils.ZipUtil;
+
+import java.io.File;
+import java.util.*;
 
 public class SimpleModuleManager implements ModuleManager {
     private final List<Module> knownModules = new ArrayList<>();
@@ -28,6 +30,43 @@ public class SimpleModuleManager implements ModuleManager {
     public void unregisterModules() {
         this.disableModules();
         this.knownModules.clear();
+    }
+
+    @Override
+    public void loadModule(File file) {
+        String plugin = ZipUtil.getFile(file.getAbsolutePath(), "plugin.yml");
+        if (plugin == null || plugin.isEmpty())
+            return;
+
+        if (!YamlUtil.getNodes(plugin).contains("module"))
+            return;
+        if (!YamlUtil.getNodes(plugin, "module").contains("main"))
+            return;
+
+        try {
+            Class<?> superClazz = this.getClass().getClassLoader().loadClass(YamlUtil.getValueByKey(plugin, "module.main").toString()).getSuperclass();
+            while (superClazz != Object.class) {
+                if (superClazz == Module.class)
+                    break;
+
+                superClazz = superClazz.getSuperclass();
+            }
+            if (superClazz != Module.class)
+                return;
+
+            this.registerModule((Module) superClazz.newInstance());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void loadModules(File folder) {
+        if (!folder.isDirectory())
+            return;
+
+        for (File file : Objects.requireNonNull(folder.listFiles()))
+            this.loadModule(file);
     }
 
     @Override
