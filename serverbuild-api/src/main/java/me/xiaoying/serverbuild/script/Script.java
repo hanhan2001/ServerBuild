93d749d9af1bdf426b09b06886e781907f12cca6
hanhan2001 <764932129@qq.com>
2025-03-12 21:04:54 +0800

    feat: make new methods of Script#registerScript and Script#getScripts

--- serverbuild-api/src/main/java/me/xiaoying/serverbuild/script/Script.java
+++ serverbuild-api/src/main/java/me/xiaoying/serverbuild/script/Script.java
@@ -2,10 +2,36 @@ package me.xiaoying.serverbuild.script;
 
 import org.bukkit.command.CommandSender;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Script
  */
 public abstract class Script {
+    private final List<Script> scripts = new ArrayList<>();
+
+    /**
+     * Register script as sub script of this command
+     *
+     * @param script Script
+     */
+    public void registerScript(Script script) {
+        if (this.scripts.contains(script))
+            return;
+
+        this.scripts.add(script);
+    }
+
+    /**
+     * Get registered sub scripts
+     *
+     * @return ArrayList
+     */
+    public List<Script> getScripts() {
+        return this.scripts;
+    }
+
     /**
      * Get script's name<br>
      *
