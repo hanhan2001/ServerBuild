b01da106cb0c854b4e85e663cd8a087645e879a8
hanhan2001 <764932129@qq.com>
2025-03-15 14:27:00 +0800

    feat: make new methods of Command#description and Command#permission

--- serverbuild-api/src/main/java/me/xiaoying/serverbuild/command/Command.java
+++ serverbuild-api/src/main/java/me/xiaoying/serverbuild/command/Command.java
@@ -11,6 +11,37 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Command {
+    /**
+     * Command's names</br>
+     * If sender called command's name not equal any name of the command's name will skip this command.
+     *
+     * @return Command's names
+     */
     String[] values();
+
+    /**
+     * Command's length<br>
+     * If sender called command length not equal this length will print help message.<br>
+     * Allow sender call the command when sender called command length equal any length<br>
+     * No limit if length is -1<br>
+     *
+     * @return Command's length
+     */
     int[] length();
+
+    /**
+     * Command's description
+     *
+     * @return command's description
+     */
+    String description() default "";
+
+    /**
+     * Get command's permission<br>
+     * Allow sender call the command when sender has any permission<br>
+     * No limit if sender is Admin
+     *
+     * @return command's permissions
+     */
+    String[] permission() default "";
 }
