957b0d36e06c8ef2a14525596e599818a24bddaa
hanhan2001 <764932129@qq.com>
2025-03-24 20:33:38 +0800

    feat: change modify of variable which in RegisteredCommand

--- serverbuild-api/src/main/java/me/xiaoying/serverbuild/command/RegisteredCommand.java
+++ serverbuild-api/src/main/java/me/xiaoying/serverbuild/command/RegisteredCommand.java
@@ -4,8 +4,8 @@ package me.xiaoying.serverbuild.command;
  * Command RegisteredCommand
  */
 public class RegisteredCommand {
-    int length;
-    SCommand subCommand;
+    private final int length;
+    private final SCommand subCommand;
 
     public RegisteredCommand(int length, SCommand subCommand) {
         this.length = length;
