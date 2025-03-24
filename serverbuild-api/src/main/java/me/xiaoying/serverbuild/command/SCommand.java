4fd475044bd4d8c2b08f2e0b2eba02d3fdeeadf0
hanhan2001 <764932129@qq.com>
2025-03-24 22:55:20 +0800

    feat: default method of SCommand#gethelpMessage

--- serverbuild-api/src/main/java/me/xiaoying/serverbuild/command/SCommand.java
+++ serverbuild-api/src/main/java/me/xiaoying/serverbuild/command/SCommand.java
@@ -1,5 +1,7 @@
 package me.xiaoying.serverbuild.command;
 
+import me.xiaoying.serverbuild.common.ConfigCommon;
+import me.xiaoying.serverbuild.factory.VariableFactory;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
@@ -13,7 +15,28 @@ import java.util.*;
  * Command SubCommand
  */
 public abstract class SCommand {
+    private SCommand parent = null;
+
     private final Map<String, List<RegisteredCommand>> registeredCommands = new HashMap<>();
+
+    /**
+     * Get parent of this command
+     *
+     * @return Parent command
+     */
+    public SCommand getParent() {
+        return this.parent;
+    }
+
+    /**
+     * Set the command's parent command<br>
+     * eg: sb reload "sb" is parent for "reload"
+     *
+     * @param command parent command
+     */
+    public void setParent(SCommand command) {
+        this.parent = command;
+    }
 
     /**
      * Get registered commands
@@ -49,7 +72,7 @@ public abstract class SCommand {
     /**
      * Get command set value
      *
-     * @return
+     * @return command's names
      */
     public List<String> getValues() {
         Command command = this.getClass().getAnnotation(Command.class);
@@ -75,7 +98,17 @@ public abstract class SCommand {
      */
     public String getDescription() {
         Command command = this.getClass().getAnnotation(Command.class);
-        return command.description();
+        return command.description().isEmpty() ? ConfigCommon.SETTING_COMMAND_MISSING_DESCRIPTION : command.description();
+    }
+
+    /**
+     * Get command's parameters
+     *
+     * @return command's parameters
+     */
+    public List<String> getParameters() {
+        Command command = this.getClass().getAnnotation(Command.class);
+        return new ArrayList<>(Arrays.asList(command.parameters()));
     }
 
     /**
@@ -86,19 +119,126 @@ public abstract class SCommand {
     public List<String> getHelpMessage() {
         List<String> list = new ArrayList<>();
 
-        // master command
+        List<String> values = this.getValues();
+        // master
+        VariableFactory masterFactory = new VariableFactory(ConfigCommon.SETTING_COMMAND_MAIN);
 
+        String commandHead = this.getValues().get(0);
+        SCommand parent = this.getParent();
 
+        while (parent != null) {
+            commandHead = values.get(0) + " " + commandHead;
+            parent = parent.getParent();
+        }
 
+        masterFactory = masterFactory.command(commandHead).description(this.getDescription());
 
-        return null;
+        // alias
+        List<String> alias = new ArrayList<>();
+        for (int i = 1; i < values.size(); i++)
+            alias.add(values.get(i));
+
+        masterFactory = masterFactory.command_alias(alias);
+
+        // usage
+        if (this.getRegisteredCommands().isEmpty()) {
+            List<String> parameters = this.getParameters();
+            StringBuilder parameterBuilder = new StringBuilder();
+
+            VariableFactory usageFactory = parameters.isEmpty() || (parameters.size() == 1 && parameters.get(0).isEmpty()) ? new VariableFactory(ConfigCommon.SETTING_COMMAND_USAGE_MISSING_PARAMETER) : new VariableFactory(ConfigCommon.SETTING_COMMAND_USAGE_PARAMETER);
+
+            // get the biggest length of this command
+            int biggest = 0;
+            for (Integer length : this.getLengths()) {
+                if (length < biggest)
+                    continue;
+
+                biggest = length;
+            }
+
+            for (int i = 0; i < biggest; i++) {
+                parameterBuilder.append(new VariableFactory(ConfigCommon.SETTING_COMMAND_PARAMETER_DEFAULT).parameter(parameters.get(i)));
+
+                if (i >= parameters.size())
+                    continue;
+
+                parameterBuilder.append(" ");
+            }
+
+            list.add(masterFactory.prefix(ConfigCommon.OVERALL_SITUATION_VARIABLE_PREFIX)
+                    .date(ConfigCommon.OVERALL_SITUATION_VARIABLE_DATEFORAMT)
+                    .command_usage(usageFactory.command(commandHead).parameter(parameterBuilder.toString()).toString())
+                    .color()
+                    .toString());
+            return list;
+        }
+
+        List<String> usageList = new ArrayList<>();
+        for (String s : this.getRegisteredCommands().keySet()) {
+            StringBuilder parameterBuilder = new StringBuilder();
+
+            // get the biggest length of the command
+            int biggest = 0;
+            List<String> parameters = new ArrayList<>();
+            String description = "";
+            for (RegisteredCommand command : this.getRegisteredCommands().get(s)) {
+                if (command.getLength() == -1) {
+                    biggest = -1;
+                    parameters = command.getSubCommand().getParameters();
+                    description = command.getSubCommand().getDescription();
+                    break;
+                }
+
+                if (command.getLength() < biggest)
+                    continue;
+
+                biggest = command.getLength();
+                parameters = command.getSubCommand().getParameters();
+                description = command.getSubCommand().getDescription();
+            }
+
+            VariableFactory usageFactory = parameters.isEmpty() || (parameters.size() == 1 && parameters.get(0).isEmpty()) ? new VariableFactory(ConfigCommon.SETTING_COMMAND_USAGE_MISSING_PARAMETER) : new VariableFactory(ConfigCommon.SETTING_COMMAND_USAGE_PARAMETER);
+
+            if (biggest == -1)
+                parameterBuilder.append(ConfigCommon.SETTING_COMMAND_PARAMETER_INFINITY);
+            else {
+                for (int i = 0; i < biggest; i++) {
+                    if (parameters.isEmpty())
+                        break;
+
+                    String parameter = parameters.get(i);
+
+                    if (!parameter.isEmpty())
+                        parameterBuilder.append(new VariableFactory(ConfigCommon.SETTING_COMMAND_PARAMETER_DEFAULT).parameter(parameter));
+
+                    if (i >= parameters.size() - 1) {
+                        int result = Math.abs(biggest - parameters.size());
+                        if (result != 0)
+                            parameterBuilder.append(" ").append(new VariableFactory(ConfigCommon.SETTING_COMMAND_PARAMETER_MISSING).amount(Math.abs(biggest - parameters.size())));
+                        break;
+                    }
+
+                    parameterBuilder.append(" ");
+                }
+            }
+
+            usageList.add(usageFactory.command(commandHead + " " + s).parameter(parameterBuilder.toString()).description(description).toString());
+        }
+
+        list.add(masterFactory.prefix(ConfigCommon.OVERALL_SITUATION_VARIABLE_PREFIX)
+                .date(ConfigCommon.OVERALL_SITUATION_VARIABLE_DATEFORAMT)
+                .command_usage(usageList)
+                .color()
+                .toString());
+
+        return list;
     }
 
     /**
      * Perform command
      *
      * @param sender Sender
-     * @param args Command's functions
+     * @param args   Command's functions
      */
     public abstract void performCommand(CommandSender sender, String[] args);
 
@@ -106,9 +246,9 @@ public abstract class SCommand {
      * Perform tab<br>
      * If you don't want sender get command help message, you can override this method and return empty list
      *
-     * @param sender Sender
+     * @param sender  Sender
      * @param command Command
-     * @param head Command head
+     * @param head    Command head
      * @param strings Command's functions
      * @return ArrayList
      */
@@ -122,7 +262,7 @@ public abstract class SCommand {
                 conditionList.add(s1);
             }
 
-            if (conditionList.size() == 0)
+            if (conditionList.isEmpty())
                 return list;
             return conditionList;
         }
