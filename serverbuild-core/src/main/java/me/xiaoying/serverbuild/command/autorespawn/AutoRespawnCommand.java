37b90e17241dfc6a996c6b6fdb1ad7d19a5b628f
hanhan2001 <764932129@qq.com>
2025-03-28 10:28:18 +0800

    feat: remove help message options in AutoRespawn.yml to support new version of SCommand

--- serverbuild-core/src/main/java/me/xiaoying/serverbuild/command/autorespawn/AutoRespawnCommand.java
+++ serverbuild-core/src/main/java/me/xiaoying/serverbuild/command/autorespawn/AutoRespawnCommand.java
@@ -5,8 +5,6 @@ import me.xiaoying.serverbuild.command.RegisteredCommand;
 import me.xiaoying.serverbuild.command.SCommand;
 import me.xiaoying.serverbuild.command.autorespawn.commands.ARReloadCommand;
 import me.xiaoying.serverbuild.command.autorespawn.commands.ARRespawnPlayerCommand;
-import me.xiaoying.serverbuild.factory.VariableFactory;
-import me.xiaoying.serverbuild.file.FileAutoRespawn;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
@@ -19,17 +17,6 @@ public class AutoRespawnCommand extends SCommand {
     public AutoRespawnCommand() {
         this.registerCommand(new ARReloadCommand());
         this.registerCommand(new ARRespawnPlayerCommand());
-    }
-
-    @Override
-    public List<String> getHelpMessage() {
-        List<String> list = new ArrayList<>();
-        list.add(new VariableFactory(FileAutoRespawn.MESSAGE_HELP)
-                .prefix(FileAutoRespawn.SETTING_PREFIX)
-                .date(FileAutoRespawn.SETTING_DATEFORMAT)
-                .color()
-                .toString());
-        return list;
     }
 
     @Override
--- serverbuild-core/src/main/java/me/xiaoying/serverbuild/file/FileAutoRespawn.java
+++ serverbuild-core/src/main/java/me/xiaoying/serverbuild/file/FileAutoRespawn.java
@@ -11,8 +11,7 @@ public class FileAutoRespawn extends SFile {
     public static String MESSAGE_RELOAD,
             MESSAGE_MISSING_PERMISSION,
             MESSAGE_NOT_FOUND_PLAYER,
-            MESSAGE_COMPLETE,
-            MESSAGE_HELP;
+            MESSAGE_COMPLETE;
 
     public static String AUTO_RESPAWN_TYPE;
 
@@ -41,7 +40,6 @@ public class FileAutoRespawn extends SFile {
         FileAutoRespawn.MESSAGE_MISSING_PERMISSION = ConfigCommon.OVERALL_SITUATION_ENABLE && ConfigCommon.OVERALL_SITUATION_MESSAGE_ENABLE ? ConfigCommon.OVERALL_SITUATION_MESSAGE_RELOAD : this.getConfiguration().getString("Message.MissingPermission");
         FileAutoRespawn.MESSAGE_NOT_FOUND_PLAYER = ConfigCommon.OVERALL_SITUATION_ENABLE && ConfigCommon.OVERALL_SITUATION_MESSAGE_ENABLE ? ConfigCommon.OVERALL_SITUATION_MESSAGE_RELOAD : this.getConfiguration().getString("Message.NotFoundPlayer");
         FileAutoRespawn.MESSAGE_COMPLETE = ConfigCommon.OVERALL_SITUATION_ENABLE && ConfigCommon.OVERALL_SITUATION_MESSAGE_ENABLE ? ConfigCommon.OVERALL_SITUATION_MESSAGE_COMPLETE : this.getConfiguration().getString("Message.Complete");
-        FileAutoRespawn.MESSAGE_HELP = this.getConfiguration().getString("Message.Help");
 
         FileAutoRespawn.AUTO_RESPAWN_TYPE = this.getConfiguration().getString("AutoRespawn.Type");
 
