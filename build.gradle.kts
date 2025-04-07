c104f81ac7b369f47a4e54a02b5db63fe800dfbd
hanhan2001 <764932129@qq.com>
2025-04-07 12:59:37 +0800

    style: remove useless codes

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,10 +12,5 @@ group = "me.xiaoying.serverbuild"
 version = "1.0.1"
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
 
-tasks.test {
-    useJUnitPlatform()
 }
